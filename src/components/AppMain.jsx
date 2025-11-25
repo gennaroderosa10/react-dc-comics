25c1c95993c54a86c096cb20fd3b00775b0020e6
gennaroderosa10 <gennaroderosa5@gmail.com>
2025-11-25 17:01:10 +0100

    feat: main done

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,3 +1,11 @@
+import style from "./Main.module.css";
+import DigitalComics from "../assets/img/buy-comics-digital-comics.png";
+import Merc from "../assets/img/buy-comics-merchandise.png";
+import Sub from "../assets/img/buy-comics-subscriptions.png";
+import Location from "../assets/img/buy-comics-shop-locator.png";
+import Visa from "../assets/img/buy-dc-power-visa.svg";
+
+
 export default function AppMain() {
     return (
         <main>
@@ -8,7 +16,30 @@ export default function AppMain() {
             </div>
             <div className="background-blue">
                 <div className="container d-flex">
-                    <h2 className="color-white py-50">--CONTENT GOES HERE--</h2>
+                    <nav className="d-flex">
+                        <ul className={style.menu}>
+                            <li className={style.icon}>
+                                <img src={DigitalComics} alt="" />
+                                PROVA
+                            </li>
+                            <li className={style.icon}>
+                                <img src={Merc} alt="" />
+                                PROVA
+                            </li>
+                            <li className={style.icon}>
+                                <img src={Sub} alt="" />
+                                PROVA
+                            </li>
+                            <li className={style.icon}>
+                                <img src={Location} alt="" />
+                                PROVA
+                            </li>
+                            <li className={style.icon}>
+                                <img src={Visa} alt="" />
+                                PROVA
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
         </main>
